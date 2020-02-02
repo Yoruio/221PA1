bbc586b9d91c045468ca2d229e0444ba29b17d10
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-02-02 14:02:30 -0800

    sandwich v

--- pa1/gridlist.cpp
+++ pa1/gridlist.cpp
@@ -147,7 +147,8 @@ void GridList::Sandwich_V(GridList& inner)
 
   GridNode* thisNode = northwest;
   int middle = dimy / 2;
-  for(int i = 1; i < middle; i++){
+  cout << "middle: " << middle;
+  for(int i = 0; i < middle; i++){
     for(int j = 0; j < dimx; j++){
       thisNode = thisNode->next;
     }
